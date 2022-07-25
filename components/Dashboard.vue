c74902a774f1e2c6de9eaf782c10210e73a327ff
PathonScript <pathonscript@gmail.com>
2022-07-25 18:53:11 +0700

    Basic setup and Dashboard

---
+++ components/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="fullflex">
+    <h1 class="text-7xl fontle">Communiquette</h1>
+    <div class="flex flex-row mt-6">
+      <h1 class="card text-2xl">Checker</h1>
+      <h1 class="card text-2xl">Daily Report</h1>
+      <h1 class="card text-2xl">Monthly Report</h1>
+</div>
+  </div>
+</template>
